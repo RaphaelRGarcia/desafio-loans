0efe8be8be675222648b84623891b039d0917444
Raphael Garcia <raphaelreisflamengo@gmail.com>
2023-08-31 11:22:29 -0300

    criação do model de Loan.java

---
+++ src/main/java/com/loans/loans/Model/Loan.java
@@ -0,0 +1,15 @@
+package com.loans.loans.Model;
+
+import java.math.BigDecimal;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Loan {
+    private String type;
+    private BigDecimal interest_rate;
+}
