b1dc9237bb31fa4cbb2428a3c05d21b28930e3ef
Raphael Garcia <raphaelreisflamengo@gmail.com>
2023-08-31 11:35:41 -0300

    criação de controllers e CostumerController.java e inserção de @NoArgsContructor em Offers.java

---
+++ src/main/java/com/loans/loans/controllers/CostumerLoans.java
@@ -0,0 +1,28 @@
+package com.loans.loans.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.loans.loans.DTOs.CostumerDTO;
+import com.loans.loans.Model.Loan;
+import com.loans.loans.Model.Offers;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("/costumer-loans")
+public class CostumerLoans {
+    @PostMapping
+    public ResponseEntity<Offers> postMethodName(@RequestBody CostumerDTO data) {
+        //Função em progresso
+        return new ResponseEntity<>(new Offers(),HttpStatus.OK);
+    }
+    
+}
