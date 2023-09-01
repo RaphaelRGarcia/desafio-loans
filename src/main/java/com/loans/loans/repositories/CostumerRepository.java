fca36f5c0f562af0e4278abda315d64fe99eda19
Raphael Garcia <raphaelreisflamengo@gmail.com>
2023-09-01 09:38:57 -0300

    criação do package repositories e CostumerRepository.java

---
+++ src/main/java/com/loans/loans/repositories/CostumerRepository.java
@@ -0,0 +1,17 @@
+package com.loans.loans.repositories;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.loans.loans.domain.User.Costumer;
+import java.util.List;
+
+
+public interface CostumerRepository extends JpaRepository<Costumer,UUID>{
+    
+    Optional<Costumer> findByCpf(String cpf);
+
+    List<Costumer> findAll();
+}
